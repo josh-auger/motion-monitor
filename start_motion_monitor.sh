3c65350b3f00cd7d2a302dbc54361aca8cb181f4
jauger <jauger@bu.edu>
2024-02-15 14:00:45 -0500

    Update docker image name

--- start_motion_monitor.sh
+++ start_motion_monitor.sh
@@ -7,11 +7,11 @@ LOG_DIR="/home/jauger/Radiology_Research/SLIMM_data/20240209_SLIMM_logs/"
 LOG_FILE=$1
 
 # Build the Docker image, if it doesn't exist
-docker build -t log-motion-monitor .
+docker build -t jauger/log-motion-monitor .
 
 # Run the Docker container with the specified log file path and remove it after completion
 docker run --rm \
   -v $LOG_DIR:/data \
-  log-motion-monitor:latest \
+  jauger/log-motion-monitor:latest \
   $LOG_FILE
 
