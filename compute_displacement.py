81cd4f343118cfe720f223ecd0ea9575850b1cc2
jauger <jauger@bu.edu>
2024-02-26 14:14:51 -0500

    Update comments

--- compute_displacement.py
+++ compute_displacement.py
@@ -91,15 +91,11 @@ def compute_displacement(transform1, transform2, outputfile=None):
 
     # Original method: l1 norm
     # displacement = abs(parms[0]*radius) + abs(parms[1]*radius) + \
-    #     abs(parms[2]*radius) + abs(parms[3]) + abs(parms[4]) + abs(parms[5])  # calculate l1 norm
+    #     abs(parms[2]*radius) + abs(parms[3]) + abs(parms[4]) + abs(parms[5])
 
-    # # Second method: l2 norm of translations
-    # displacement = (abs(parms[0] * radius) + abs(parms[1] * radius) + abs(parms[2] * radius) + \
-    #                 np.sqrt((parms[3]**2) + (parms[4]**2) + (parms[5]**2)))  # calculate l2 norm
-
-    # Third method: l2 norm of translations and rotations-ish
+    # Alternative method: arc length of magnitude of rotation + l2 norm of translations
     displacement = (radius * np.sqrt((parms[0]**2) + (parms[1]**2) + (parms[2]**2))) + \
-                    np.sqrt((parms[3] ** 2) + (parms[4] ** 2) + (parms[5] ** 2)))  # calculate l2 norm
+                    np.sqrt((parms[3] ** 2) + (parms[4] ** 2) + (parms[5] ** 2))
 
     print("\nDisplacement : ", displacement)
 
