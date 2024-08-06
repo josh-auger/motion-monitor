2b5f8e518edb283b55cc2f47787fd4edb65588d2
jauger <jauger@bu.edu>
2024-08-06 14:48:26 -0400

    Add proxy permissions to build command

--- build_docker_motion_monitor.sh
+++ build_docker_motion_monitor.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build --rm -t jauger/motion-monitor:latest -f ./Dockerfile .
+#docker build --rm -t jauger/motion-monitor:latest -f ./Dockerfile .
+docker build --rm --build-arg HTTP_PROXY=http://proxy.tch.harvard.edu:3128 -t jauger/motion-monitor:latest -f ./Dockerfile .
